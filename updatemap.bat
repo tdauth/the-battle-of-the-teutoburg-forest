8de18ea8d729fe46c6b4c85b86f004353395465b
Tamino Dauth <tamino@cdauth.de>
2025-03-16 18:42:13 +0100

    update

--- updatemap.bat
+++ updatemap.bat
@@ -1,4 +1,4 @@
-set version="1.2"
+set version="1.3"
 set dir="C:\Users\Tamino\Documents\Projekte\the-battle-of-the-teutoburg-forest\releases"
 set map=%dir%\bottf%version%.w3x
 copy %map% "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
